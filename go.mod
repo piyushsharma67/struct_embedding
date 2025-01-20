f0fcb2af4d8fa67c50cc5891c02bac985bf95cd1
piyushsharma67 <piyush67.sharma@gmail.com>
2025-01-20 21:59:01 +0530

    structs embeding in go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module struct_embedding
+
+go 1.23.5
---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+type SpecialPosition struct{}
+
+
+func (sp *SpecialPosition)TravelSpecialPosition(){
+	fmt.Println("i have travelled to special position")
+}
+
+type Position struct{
+	x float64
+	y float64
+}
+
+func (p *Position)movePlayer(x,y float64){
+	p.x+=x
+	p.y+=y
+}
+
+func (p *Position)teleportPlayer(x,y float64){
+	p.x=x
+	p.y=y
+}
+
+type Player struct{
+	Position
+	SpecialPosition // embedded two structs inside the Plaer struct hence getting their attached properties and methods
+}
+
+
+func NewPlayer()*Player{
+	return &Player{
+		Position:Position{},
+	}
+}
+
+func main(){
+	player:=NewPlayer()
+	player.movePlayer(12,13)
+	fmt.Println(player.Position)
+	player.TravelSpecialPosition()
+}
